f27c26fd5c2909d010d82bb334008bdf023cd122
id2k1149 <id2k1149@gmail.com>
2021-09-07 08:26:41 -0400

    add AdminRestaurantController

--- src/main/java/com/project/votingApp/repository/DishRepository.java
+++ src/main/java/com/project/votingApp/repository/DishRepository.java
@@ -1,11 +1,13 @@
 package com.project.votingApp.repository;
 
 import com.project.votingApp.model.Dish;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 @Transactional(readOnly = true)
 public interface DishRepository extends BaseRepository<Dish> {
     List<Dish> findAllByDate(LocalDate date);
--- src/main/java/com/project/votingApp/repository/UserRepository.java
+++ src/main/java/com/project/votingApp/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.project.votingApp.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.project.votingApp.model.User;
 
 import java.util.Optional;
 
+@Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends BaseRepository<User> {
     Optional<User> getByEmail(String email);
